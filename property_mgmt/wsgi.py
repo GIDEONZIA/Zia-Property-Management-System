fd3a7a439a5f7aa5861a072a2680f55a9077b483
GIDEONZIA <gideonkahindi9329@gmail.com>
2026-06-17 15:11:31 +0300

    Fix wsgi dynamic attribute error and map valid installCommand structure

--- property_mgmt/wsgi.py
+++ property_mgmt/wsgi.py
@@ -14,3 +14,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'property_mgmt.settings')
 
 application = get_wsgi_application()
+
+# Safe comment cache break trigger 2026
